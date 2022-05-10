94c4e5ffdc288a91f836060ee80e273f4cb05ccd
Joshita <84522681+Joshita-2009@users.noreply.github.com>
2022-05-10 19:55:43 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,12 @@
+var player1_name= localStorage.getItem("player1_name");
+var player2_name= localStorage.getItem("player2_name");
+var player1_score= 0;
+var player2_score= 0;
+
+document.getElementById("player1_name").innerHTML = player1_name;
+document.getElementById("player2_name").innerHTML = player2_name;
+document.getElementById("player1_score").innerHTML =  player1_score;
+document.getElementById("player2_score").innerHTML =  player2_score;
+
+document.getElementById("question_turn").innerHTML= "Question turn - " + player1_name;
+document.getElementById("answer_turn").innerHTML= "Answer_turn - " + player2_name;
